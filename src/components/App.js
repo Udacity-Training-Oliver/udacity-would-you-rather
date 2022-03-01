0c17ac2fe09eee1db46f778faf8d5abb17d2f03b
Oliver Brandt <oliver.brandt@hamburg.de>
2022-03-01 21:40:27 +0100

    feat: Add component skeleton without styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
 import React, { useEffect, Fragment } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { 
+  BrowserRouter as Router, 
+  Route, 
+  Routes,
+  Link
+} from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import LoadingBar from 'react-redux-loading';
 import { handleInitialData } from '../actions/shared';
+import HomePage from './Home/HomePage';
+import AddQuestionPage from './Questions/AddQuestionPage';
+import LeaderBoardPage from './LeaderBoard/LeaderBoardPage';
+import LoginPage from './Login/LoginPage';
 
 export default function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(handleInitialData());
-  });
+  }, [dispatch]);
 
   const loading = useSelector((state) => state.authedUser === null);
 
@@ -17,14 +26,45 @@ export default function App() {
     <Router>
       <Fragment>
         <LoadingBar />
-        <div className='container'>
+        <div>
           {loading === true
             ? null
             :
             <div>
-              Hello "Would you rather"
+              <header>
+                <nav>
+                  <ul>
+                    <li>
+                      <Link  to="/">
+                        <span>Home</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/add">
+                        <span>Add Question</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/leaderboard">
+                        <span>Leader Board</span>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/login">
+                        <span>Login</span>
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </header>
             </div>}
         </div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/add" element={<AddQuestionPage />} />
+          <Route path="/leaderboard" element={<LeaderBoardPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </Fragment>
     </Router>
   );
---
+++ src/components/Questions/AddQuestionPage.js
@@ -0,0 +1,7 @@
+export default function AddQuestion() {
+  return (
+    <main>
+      <p>Add question!</p>
+    </main>
+  );
+}
