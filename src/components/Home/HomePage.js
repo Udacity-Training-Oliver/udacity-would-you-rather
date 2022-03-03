d4e5c599f3f15a0edda8ee38a1f020f723d13972
Oliver Brandt <oliver.brandt@hamburg.de>
2022-03-03 09:18:19 +0100

    feat: Add tab for answered/unanswered questions

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,16 @@
+import { Tab, Tabs } from "react-bootstrap";
+
 export default function Home() {
   return (
     <main>
-      <p>Home!</p>
+      <Tabs defaultActiveKey="unanswered" id="questions" className="mb-3">
+        <Tab eventKey="unanswered" title="Unanswered Questions">
+          <p>Unanswered Questions</p>
+        </Tab>
+        <Tab eventKey="answered" title="Answered Questions">
+          <p>Answered Questions</p>
+        </Tab>
+      </Tabs>
     </main>
   );
 }
