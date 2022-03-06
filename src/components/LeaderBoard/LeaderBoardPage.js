191ec8a13e9dcfa50e9ebc6a2ee84e9fe70f43d9
Oliver Brandt <oliver.brandt@hamburg.de>
2022-03-06 18:08:31 +0100

    feat: Replace dummy data by real data

--- src/components/LeaderBoard/LeaderBoardPage.js
+++ src/components/LeaderBoard/LeaderBoardPage.js
@@ -1,35 +1,22 @@
+import { useSelector } from "react-redux";
 import ScoreList from "./ScoreList"
 
 export default function LeaderBoard() {
+  const scores = useSelector((state) =>
+    Object.values(state.users).map(u => ({
+      id: u.id,
+      user: {
+        name: u.name,
+        avatarURL: u.avatarURL,
+      },
+      answeredQuestions: Object.keys(u.answers).length,
+      createdQuestions: Object.values(state.questions)
+        .filter((q) => q.author === u.id).length,
+    }))
+  );
+  console.log('Scores', scores);
 
-  const scores = [
-    {
-      id: '1',
-      user: {
-        name: 'Oliver'
-      },
-      answeredQuestions: 1,
-      createdQuestions: 2,
-    },
-    {
-      id: '2',
-      user: {
-        name: 'Lupo'
-      },
-      answeredQuestions: 0,
-      createdQuestions: 5,
-    },
-    {
-      id: '3',
-      user: {
-        name: 'Minky'
-      },
-      answeredQuestions: 2,
-      createdQuestions: 2,
-    },
-  ];
-
-   scores.sort((a, b) => (a.answeredQuestions + a.createdQuestions) - (b.answeredQuestions + b.createdQuestions));
+  scores.sort((a, b) => (b.answeredQuestions + b.createdQuestions) - (a.answeredQuestions + a.createdQuestions));
 
   return (
     <main>
