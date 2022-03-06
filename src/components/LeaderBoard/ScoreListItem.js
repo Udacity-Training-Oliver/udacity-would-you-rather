6081990209b508c4e4a04996c651ed4ce722cd5b
Oliver Brandt <oliver.brandt@hamburg.de>
2022-03-06 18:08:10 +0100

    feat: Style score card

--- src/components/LeaderBoard/ScoreListItem.js
+++ src/components/LeaderBoard/ScoreListItem.js
@@ -1,25 +1,46 @@
 import PropTypes from 'prop-types'
-import { Card } from "react-bootstrap";
+import { Container, Col, Row, Card } from "react-bootstrap";
 
 export default function ScoreListItem(props) {
   const { user, answeredQuestions, createdQuestions } = props;
 
   return (
-    <Card style={{ width: '24rem' }}>
+    <Card className='m-3' style={{ width: '34rem' }}>
       <Card.Body>
-        <Card.Title>{user.name}</Card.Title>
-        <Card.Text>
-          Answered questions: {answeredQuestions}
-        </Card.Text>
-        <Card.Text>
-          Created questions: {createdQuestions}
-        </Card.Text>
-        <Card>
-          <Card.Header as="h5">Score</Card.Header>
-          <Card.Text>
-            Total Score: {answeredQuestions + createdQuestions}
-          </Card.Text>
-        </Card>
+        <Container>
+          <Row>
+            <Col lg="2">
+              <Card.Img width={85} variant="Top" src={user.avatarURL} />
+            </Col>
+            <Col lg="6">
+              <Container>
+                <Row>
+                  <Card.Title>{user.name}</Card.Title>
+                </Row>
+                <Row className="mt-1">
+                  <Card.Text>
+                    Answered questions: {answeredQuestions}
+                  </Card.Text>
+                </Row>
+                <Row  className="mt-1">
+                  <Card.Text className='mb-2'>
+                    Created questions: {createdQuestions}
+                  </Card.Text>
+                </Row>
+              </Container>
+            </Col>
+            <Col lg="1">
+              <Card style={{ width: '5.5rem', height: "5.5rem" }}>
+                <Card.Header as="h6">Score</Card.Header>
+                <Card.Body>
+                  <Card.Text>
+                    {answeredQuestions + createdQuestions}
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </Card.Body>
     </Card>
   );
