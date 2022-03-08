7f470ecfd8be8db4270fa44b81b94fd18f445975
Oliver Brandt <oliver.brandt@hamburg.de>
2022-03-08 21:25:18 +0100

    feat: Handle 404 for invalid question URL

--- src/components/Questions/QuestionPage.js
+++ src/components/Questions/QuestionPage.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import AlreadyVotedQuestion from "./AlreadyVotedQuestion";
 import ToBeVotedQuestion from "./ToBeVotedQuestion";
@@ -7,8 +7,14 @@ export default function QuestionPage() {
   const { id } = useParams();
 
   const question = useSelector((state) => state.questions[id]);
+
   const user = useSelector((state) => state.users[state.authedUser]);
-  const author = useSelector((state) => state.users[question.author]);
+  const author = useSelector((state) => state.users[question?.author]);
+
+  if (!question) {
+    return <Navigate to="/NotFound" replace />;
+  }
+
   const answered = Object.keys(user.answers).includes(question.id);
 
   const usersVote = answered
